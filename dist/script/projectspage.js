7fa187146dfb145b405111f1230a9412ca4e8fbb
Matt Hall <matthew349hall@hotmail.com>
2019-12-06 17:54:47 -0500

    done

--- dist/script/projectspage.js
+++ dist/script/projectspage.js
@@ -1,7 +1,15 @@
 
     let projects = document.querySelectorAll('.card-shell')
     let display = document.querySelector('#project-display')
+
+    let back = display.querySelector('#back-button')
+    let displayBody = display.querySelector('#display-body')
+    let displayTitle = display.querySelector('#display-title')
     console.log(projects)
+
+    back.addEventListener('click', ()=> {
+        display.style.display = "none";
+    })
 
     projects.forEach(project => {
         project.addEventListener('click',(e) => {
@@ -17,5 +25,10 @@
         const response = await fetch(`/project/${link}`);
         const myJson = await response.json();
         console.log(JSON.stringify(myJson));
+
+        display.style.display = "block"
+        displayBody.innerHTML = myJson.body
+        displayTitle.innerHTML = myJson.title
+
     }
 
